06ca9746f80cadc9587d90d9a4442f3652e1e9e2
azfar.imtiaz <azfar.imtiaz@live.com>
2023-08-15 20:10:05 +0200

    Fetch dummy transactions from link and build TransactionList View Model

---
+++ Expense Tracker/ViewModels/TransactionListViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  TransactionListViewModel.swift
+//  Expense Tracker
+//
+//  Created by Azfar Imtiaz on 2023-08-15.
+//
+
+import Foundation
+import Combine
+
+final class TransactionListViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getTransactions()
+    }
+    
+    func getTransactions() {
+        guard let url = URL(string: "https://designcode.io/data/transactions.json") else {
+            print("Invalid URL!")
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) -> Data in
+                guard let httpResposnse = response as? HTTPURLResponse, httpResposnse.statusCode == 200 else {
+                    dump(response)
+                    throw URLError(.badServerResponse)
+                }
+                
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error while fetching transactions: ", error.localizedDescription)
+                case .finished:
+                    print("Finished fetching transactions!")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transactions = result
+            }
+            .store(in: &cancellables)
+    }
+}
